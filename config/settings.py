40a8024306f4d5152d3699f4d5204f9a7241925c
BrahmendraJayaraju <81387382+BrahmendraJayaraju@users.noreply.github.com>
2026-05-04 12:01:47 -0400

    Fix CI database and enable tests

--- config/settings.py
+++ config/settings.py
@@ -98,12 +98,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "controltower_db",
-        "USER": "controltower_user",
-        "PASSWORD": "Aa9886603727$",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
-
+        "NAME": os.getenv("DB_NAME", "controltower_db"),
+        "USER": os.getenv("DB_USER", "controltower_user"),
+        "PASSWORD": os.getenv("DB_PASSWORD", "Aa9886603727$"),
+        "HOST": os.getenv("DB_HOST", "127.0.0.1"),
+        "PORT": os.getenv("DB_PORT", "5432"),
     }
 }
 
